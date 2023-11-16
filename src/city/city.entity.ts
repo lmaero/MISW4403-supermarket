895f4c21755aab95be68654833371d0589b7e853
Luis Guzman <luis.guzman@falcani.com>
2023-11-16 07:06:44 -0500

    Add entities attributes

--- src/city/city.entity.ts
+++ src/city/city.entity.ts
@@ -1,1 +1,20 @@
-export class CityEntity {}
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { SupermarketEntity } from '../supermarket/supermarket.entity';
+
+@Entity()
+export class CityEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  population: number;
+
+  @ManyToMany(() => SupermarketEntity, (supermarket) => supermarket.cities)
+  supermarkets: SupermarketEntity[];
+}
--- src/supermarket/supermarket.entity.ts
+++ src/supermarket/supermarket.entity.ts
@@ -1,1 +1,23 @@
-export class SupermarketEntity {}
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { CityEntity } from '../city/city.entity';
+
+@Entity()
+export class SupermarketEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  longitude: string;
+
+  @Column()
+  latitude: string;
+
+  @Column()
+  webpage: string;
+
+  @ManyToMany(() => CityEntity, (city) => city.supermarkets)
+  cities: CityEntity[];
+}
