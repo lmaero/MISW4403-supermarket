fb251f6502bcc2e0a8ebd836965c6b9e492111d2
Luis Guzman <luis.guzman@falcani.com>
2023-11-16 06:55:48 -0500

    Create modules and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CityModule } from './city/city.module';
+import { SupermarketModule } from './supermarket/supermarket.module';
 
 @Module({
-  imports: [],
+  imports: [CityModule, SupermarketModule],
   controllers: [AppController],
   providers: [AppService],
 })
