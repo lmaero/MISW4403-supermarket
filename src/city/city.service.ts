74926bf22c49d517db8d67013f7084ad1e87bd93
Luis Guzman <luis.guzman@falcani.com>
2023-11-16 07:53:38 -0500

    Add services business logic

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -2,6 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CityEntity } from './city.entity';
 import { Repository } from 'typeorm';
+import {
+  BusinessError,
+  BusinessLogicException,
+} from '../shared/errors/business-errors';
+
+function verifyCountry(city: CityEntity) {
+  const countries = ['Argentina', 'Ecuador', 'Paraguay'];
+
+  if (!countries.includes(city.country)) {
+    throw new BusinessLogicException(
+      'That is not a valid country',
+      BusinessError.PRECONDITION_FAILED,
+    );
+  }
+}
 
 @Injectable()
 export class CityService {
@@ -9,4 +24,57 @@ export class CityService {
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
   ) {}
+
+  async findAll(): Promise<CityEntity[]> {
+    return await this.cityRepository.find({ relations: ['supermarkets'] });
+  }
+
+  async findOne(d: string): Promise<CityEntity> {
+    const city: CityEntity = await this.cityRepository.findOne({
+      where: { id },
+      relations: ['supermarkets'],
+    });
+
+    if (!city)
+      throw new BusinessLogicException(
+        'The city with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    return city;
+  }
+
+  async create(cty: CityEntity): Promise<CityEntity> {
+    verifyCountry(city);
+    return await thiscityRepository.save(city);
+  }
+
+  async update(i: string, city: CityEntity): Promise<CityEntity> {
+    const persistedCity: CityEntity = await this.cityRepository.findOne({
+      where: { id },
+    });
+
+    if (!persistedCiy)
+      throw new BusinessLogicException(
+        'The city with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    verifyCountry(ciy);
+    return await thiscityRepository.save({ ...persistedCity, ...city });
+  }
+
+  async delete(i: string) {
+    const city: CityEntity = await this.cityRepository.findOne({
+      where: { id },
+    });
+
+    if (!city)
+      throw new BusinessLogicException(
+        'The city with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    await this.cityRpository.remove(city);
+  }
 }
--- src/supermarket/supermarket.service.ts
+++ src/supermarket/supermarket.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SupermarketEntity } from './supermarket.entity';
+import {
+  BusinessError,
+  BusinessLogicException,
+} from '../shared/errors/business-errors';
+
+function verifySupermarketName(supermarket: SupermarketEntity) {
+  if (supermarket.name.length <= 10) {
+    throw new BusinessLogicException(
+      'Supermarket name should be at least 10 characters long',
+      BusinessError.PRECONDITION_FAILED,
+    );
+  }
+}
 
 @Injectable()
 export class SupermarketService {
@@ -9,4 +22,60 @@ export class SupermarketService {
     @InjectRepository(SupermarketEntity)
     private readonly supermarketRepository: Repository<SupermarketEntity>,
   ) {}
+
+  async findAll(): Promise<SupermarketEntity[]> {
+    return await this.supermarketRepository.find({ relations: ['cities'] });
+  }
+
+  async findOne(id: string): Promise<SupermarketEntity> {
+    const supermarket: SupermarketEntity =
+      await this.supermarketRepository.findOne({
+        where: { id },
+        relations: ['cities'],
+      });
+    if (!supermarket)
+      throw new BusinessLogicException(
+        'The supermarket with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+    return supermarket;
+  }
+
+  async create(supermarket: SupermarketEntity): Promise<SupermarketEntity> {
+    verifySupermarketName(supermarket);
+    return await this.supermarketRepository.save(supermarket);
+  }
+
+  async update(
+    id: string,
+    supermarket: SupermarketEntity,
+  ): Promise<SupermarketEntity> {
+    const persistedSupermarket: SupermarketEntity =
+      await this.supermarketRepository.findOne({
+        where: { id },
+      });
+    if (!persistedSupermarket)
+      throw new BusinessLogicException(
+        'The supermarket with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+    verifySupermarketName(supermarket);
+    return await this.supermarketRepository.save({
+      ...persistedSupermarket,
+      ...supermarket,
+    });
+  }
+
+  async delete(id: string) {
+    const supermarket: SupermarketEntity =
+      await this.supermarketRepository.findOne({
+        where: { id },
+      });
+    if (!supermarket)
+      throw new BusinessLogicException(
+        'The supermarket with the given id was not found',
+        BusinessError.NOT_FOUND,
+      );
+    await this.supermarketRepository.remove(supermarket);
+  }
 }
