73efc1611e7320743dd5fc93483c13e24f3f8b0e
Luis Guzman <luis.guzman@falcani.com>
2023-11-18 12:41:46 -0500

    Add endpoints

--- src/city-supermarket/city-supermarket.controller.ts
+++ src/city-supermarket/city-supermarket.controller.ts
@@ -1,11 +1,75 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CitySupermarketService } from './city-supermarket.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { plainToInstance } from 'class-transformer';
+import { SupermarketDto } from '../supermarket/supermarket.dto';
+import { SupermarketEntity } from '../supermarket/supermarket.entity';
 
-@Controller('cities/:cityId/supermarkets')
+@Controller('cities')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CitySupermarketController {
   constructor(
     private readonly citySupermarketService: CitySupermarketService,
   ) {}
+
+  @Post(':cityId/supermarkets/:supermarketId')
+  async addSupermarketToCity(
+    @Param('cityId') cityId: string,
+    @Param('supermarketId') supermarketId: string,
+  ) {
+    return await this.citySupermarketService.addSupermarketToCity(
+      cityId,
+      supermarketId,
+    );
+  }
+
+  @Get(':cityId/supermarkets/:supermarketId')
+  async findSupermarketFromCity(
+    @Param('cityId') cityId: string,
+    @Param('supermarketId') supermarketId: string,
+  ) {
+    return await this.citySupermarketService.findSupermarketFromCity(
+      cityId,
+      supermarketId,
+    );
+  }
+
+  @Get(':cityId/supermarkets')
+  async findSupermarketsFromCity(@Param('cityId') cityId: string) {
+    return await this.citySupermarketService.findSupermarketsFromCity(cityId);
+  }
+
+  @Put(':cityId/supermarkets')
+  async updateSupermarketsFromCity(
+    @Body() supermarketsDto: SupermarketDto[],
+    @Param('cityId') cityId: string,
+  ) {
+    const supermarkets = plainToInstance(SupermarketEntity, supermarketsDto);
+    return await this.citySupermarketService.updateSupermarketsFromCity(
+      cityId,
+      supermarkets,
+    );
+  }
+
+  @Delete(':cityId/supermarkets/:supermarketId')
+  @HttpCode(204)
+  async deleteSupermarketFromCity(
+    @Param('cityId') cityId: string,
+    @Param('supermarketId') supermarketId: string,
+  ) {
+    return await this.citySupermarketService.deleteSupermarketFromCity(
+      cityId,
+      supermarketId,
+    );
+  }
 }
